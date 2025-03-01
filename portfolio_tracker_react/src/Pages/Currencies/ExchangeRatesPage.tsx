1d420e718fab751b92502b8e8093e0772f8e0932
Burak Cinar <aburakcinar@gmail.com>
2025-03-01 01:39:03 +0100

    - FinanceData project - Time series Currency Exchange rate endpoints.

--- portfolio_tracker_react/src/Pages/Currencies/ExchangeRatesPage.tsx
+++ portfolio_tracker_react/src/Pages/Currencies/ExchangeRatesPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { PageHeader } from "../../Controls/PageHeader";
+import { Card } from "primereact/card";
 
 export const ExchangeRatesPage: React.FC = () => {
   return (
-    <div className="flex flex-row mt-20 w-full">
-      <div className="flex flex-row w-1/2 justify-center">
+    <div className="flex flex-col mt-20 w-full justify-center">
+      <div className="flex flex-col w-1/2 ">
         <PageHeader title="Exchange Rates" />
+
+        <Card title="Rates">
+          <svg></svg>
+        </Card>
       </div>
     </div>
   );
